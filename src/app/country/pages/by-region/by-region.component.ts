1c2156a07bfdc6da206d71609936e54706790e20
VirginiaKMartinez <hola@virginiak.es>
2021-11-24 18:38:21 +0100

    Sidebar, search by country, search by region, search by capital and see country component created

---
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-region',
+  templateUrl: './by-region.component.html',
+  styleUrls: ['./by-region.component.sass']
+})
+export class ByRegionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
